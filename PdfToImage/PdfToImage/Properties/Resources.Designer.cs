6b750057805669dcecbb0bc62009eb022925ea22
Zhu Jisong <zhujisong@xuantech.cn>
2021-12-28 15:59:14 +0800

    修改文件名称

--- PdfToImage/PdfToImage/Properties/Resources.Designer.cs
+++ PdfToImage/PdfToImage/Properties/Resources.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PdfToJpg.Properties {
+namespace PdfToImage.Properties {
     using System;
     
     
@@ -39,7 +39,7 @@ namespace PdfToJpg.Properties {
         internal static global::System.Resources.ResourceManager ResourceManager {
             get {
                 if (object.ReferenceEquals(resourceMan, null)) {
-                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PdfToJpg.Properties.Resources", typeof(Resources).Assembly);
+                    global::System.Resources.ResourceManager temp = new global::System.Resources.ResourceManager("PdfToImage.Properties.Resources", typeof(Resources).Assembly);
                     resourceMan = temp;
                 }
                 return resourceMan;
--- PdfToImage/PdfToImage/Properties/Settings.Designer.cs
+++ PdfToImage/PdfToImage/Properties/Settings.Designer.cs
@@ -8,7 +8,7 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-namespace PdfToJpg.Properties {
+namespace PdfToImage.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
